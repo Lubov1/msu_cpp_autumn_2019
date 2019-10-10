f98568870cc2b3f8bd2f47ee968f85fc683fee43
Lubov1 <lubov.888888@yandex.ru>
2019-10-10 14:40:25 +0300

    task00.cadd

---
+++ 00/sum_by_rows.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdint>
+#include <chrono>
+#include <iostream>
+
+class Timer
+{
+    using clock_t = std::chrono::high_resolution_clock;
+    using microseconds = std::chrono::microseconds;
+public:
+    Timer()
+        : start_(clock_t::now())
+    {
+    }
+
+    ~Timer()
+    {
+        const auto finish = clock_t::now();
+        const auto us = 
+            std::chrono::duration_cast<microseconds>
+                (finish - start_).count();
+        std::cout << us << " us" << std::endl;
+    }
+
+private:
+    const clock_t::time_point start_;
+};
+int main(){
+	volatile int sum=0;
+    int N=1000;
+    int a[N][N]={0};
+    Timer t;
+    for(int i=0;i<N;i++)
+        for(int j=0;j<N;j++)
+            sum+=a[i][j];
+
+
+
+
+}
