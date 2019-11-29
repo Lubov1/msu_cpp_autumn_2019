e011141173de3fd14e64776a6920530c12930f39
lubov <lubov.88888@yandex.ru>
2019-11-29 14:35:18 +0300

    update

---
+++ 07/Vector.h
@@ -0,0 +1,177 @@
+#include <iterator>
+#include <iostream>
+#include <stdlib.h>
+#include <exception>
+#include <stdexcept>
+
+using namespace std;
+
+template<class T>
+class Allocator
+{
+public:
+	using value_type = T;
+	using pointer = T*;
+	using size_type = size_t;
+
+	template<class... ArgsT>
+	void construct(pointer ptr, ArgsT&& ... Args){
+		new (ptr)T(std::forward<ArgsT>(Args)...);
+	}
+	pointer allocate(size_type size){
+		return new value_type[size];
+	}
+	void deallocate(pointer ptr, size_type size){
+		delete[](ptr);
+	}
+	void destroy(pointer ptr){
+		ptr->~T();
+
+	}
+};
+
+
+template <class T>
+class Iterator
+	: public iterator<random_access_iterator_tag, T>
+{
+	T* ptr_;
+public:
+	using reference = T&;
+	explicit Iterator(T* ptr)
+		: ptr_(ptr){
+	}
+
+	bool operator==(const Iterator<T>& other) const{
+		return ptr_ == other.ptr_;
+	}
+
+	bool operator!=(const Iterator<T>& other) const{
+		return !(*this == other);
+	}
+
+	reference operator*() const{
+		return *ptr_;
+	}
+
+	Iterator& operator++(){
+		++ptr_;
+		return *this;
+	}
+	Iterator& operator--(){
+		--ptr_;
+		return *this;
+	}
+};
+
+template <class T, class Alloc = Allocator<T>>
+class Vector
+{
+public:
+	using iterator = Iterator<T>;
+
+	using size_type = size_t;
+	using value_type = T;
+	using reference = T&;
+	using const_reference = const T&;
+	using allocator_type = Alloc;
+	using pointer = T*;
+	size_type allocated;
+	T* data;
+	size_type used;
+	explicit Vector(size_type count = 0){
+		used = count;
+		data = alloc.allocate(count * 2);
+		allocated = count * 2;
+		if (used != 0)
+			for (pointer p = data; p != data + count; ++p){
+				alloc.construct(p);
+			}
+	}
+	explicit Vector(size_type count, const_reference value){
+		used = count;
+		data = alloc.allocate(count * 2);
+		allocated = count * 2;
+
+		for (pointer p = data; p != data + count; ++p){
+			alloc.construct(p,value);
+		}
+	}
+	iterator begin() noexcept{
+		return iterator(data);
+	}
+	reverse_iterator<T> rbegin() noexcept{
+		return reverse_iterator<T>(iterator(data));
+	}
+
+	iterator end() noexcept{
+		return iterator(data+used);
+
+	}
+	reverse_iterator<T> rend() noexcept{
+		return reverse_iterator<T>(iterator(data));
+	}
+
+	size_type size() const{
+		return used;
+	}
+
+	void push_back(const value_type& value){
+		if (used + 1 > allocated)
+			reserve(allocated*2);
+		alloc.construct(data + (used)++, value);
+	}
+	value_type pop_back(){
+		used--;
+		value_type el = data[used];
+		alloc.destroy(data + used);
+		return el;
+	}
+	reference operator[](size_type pos){
+		return data[pos];
+	}
+	bool empty()const{
+		return this->used == 0;
+	}
+
+	void reserve(size_type count){
+		pointer data1;
+		if (count > allocated){
+			data1 = alloc.allocate(count);
+			for (pointer p = data1; p < data1 + used; p++){
+				alloc.construct(p, move(*p));
+				alloc.destroy(p);
+			}
+			alloc.deallocate(data, used);
+			allocated = count;
+			data = data1;
+
+		}
+	}
+	void resize(size_type newSize){
+		if (newSize > used){
+			if(newSize >= allocated)
+				alloc.reserve(newSize);
+			for (pointer p = used+1; p<newSize; p++)
+				alloc.construct(p, T());
+		}
+		if (newSize < used){
+			for (pointer p = data + used; p > data + newSize; p--)
+				alloc.destroy(p);
+		}
+		used = newSize;
+	}
+	void clear() noexcept{
+		for (pointer p = data; p < data + used; p++)
+			alloc.destroy(p);
+		used = 0;
+	}
+	~Vector(){
+		for (pointer p = data; p != data + used; ++p)
+			alloc.destroy(p);
+		alloc.deallocate(data, allocated);
+	}
+	private:
+		Alloc alloc;
+};
+
---
+++ 07/test.cpp
@@ -0,0 +1,35 @@
+#include <iterator>
+#include <iostream>
+#include <stdlib.h>
+#include <exception>
+#include <stdexcept>
+#include <assert.h>
+#include <string>
+
+#include "Vector.h"
+
+using namespace std;
+
+
+int main(){
+	size_t size = 4;
+	Vector<int> v(size, 7);
+	for (int i = 0; i < v.size(); i++)
+		assert(v[i] == 7);
+	v.push_back(23);
+	assert(size + 1 == v.size());
+	assert(v[v.size() - 1] == v.pop_back());
+	v[0] = 6;
+	assert(v[0] == 6);
+	Vector<int> v1(3);
+	Iterator<int> t = v1.begin();
+	for (int j = 0; j < v1.size();j++,++t)
+		assert(v1[j] == *t);
+
+	Vector<string> v3(5,"yes");
+	Iterator<string> t3 = v3.begin();
+
+	for (int j = 0; j < v3.size();j++,++t3)
+		assert(v3[j] == *t3);
+		
+}
